0e130d650c0efcad37d2a399507a97f3beb2b4bc
Jakob Schreiner <jakob2602@gmail.com>
2020-01-10 11:24:52 +0100

    moved scripts to entrypoints

--- setup.py
+++ setup.py
@@ -16,5 +16,13 @@ setup(
         "imutils",
         "pylint",
         "scipy"
-    ]
+    ],
+    entry_points={
+        "console_scripts": [
+            "split-image = scripts.split_image:main",
+            "segment-traces = scripts.extract_traces:main",
+            "digitise-traces = scripts.digitise_traces:main",
+            "plot-trace = scripts.plot_array:main",
+        ]
+    }
 )
